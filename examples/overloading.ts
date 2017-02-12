41834b910879d3af0fa431faa708bf3b642a16c3
arusakov <a_s_rusakov@mail.ru>
2017-02-12 18:27:59 +0300

    overloading

--- examples/overloading.ts
+++ examples/overloading.ts
@@ -2,19 +2,11 @@
 // part 1
 //
 
+document.createElement('a') // HTMLAnchorElement
+document.createElement('div') // HTMLDivElement
+
 interface Overloads {
     createElement(tagName: 'a'): HTMLAnchorElement
     // ...
     createElement(tagName: string): HTMLElement
-
-
-    addEventListener(
-        type: 'keydown',
-        listener: (this: this, ev: KeyboardEvent) => any,
-        useCapture?: boolean): void
-    // ...
-    addEventListener(
-        type: string,
-        listener: EventListenerOrEventListenerObject,
-        useCapture?: boolean): void
 }
